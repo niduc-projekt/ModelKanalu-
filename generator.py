a51529f1bf16193b0abd09fdeedea10a97f56aba
Vyctrus <37441039+Vyctrus@users.noreply.github.com>
2019-03-20 16:37:53 +0100

    Add files via upload

--- generator.py
+++ generator.py
@@ -1,7 +1,10 @@
-import random, sys
+def generate_f():
+    import random, sys
 
-with open("DataGenerated.txt", 'w') as file:
+    with open("DataGenerated.txt", 'w') as file:
 
-    amount = 500        #int(sys.argv[1])
-    for i in range(0, amount):
-        file.write(str(random.randint(0, 1)))
+        amount = 1000        #int(sys.argv[1])
+        for i in range(0, amount):
+            file.write(str(random.randint(0, 1)))
+
+print("Witam z symulacji");
---
+++ symulation.py
@@ -0,0 +1,11 @@
+import generator
+import Tripling_codder
+import Channel
+import Tripling_decoder
+import ResultsTable
+
+generator.generate_f()
+Tripling_codder.tripling_code()
+Channel.passing()
+Tripling_decoder.tripling_decode()
+ResultsTable.scores()
